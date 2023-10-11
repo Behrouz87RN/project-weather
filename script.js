db7caffe5c95bf66d70bdc0eeeb84414c1041a3c
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-11 12:13:46 +0200

    get city name in UI

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
+// Function to generate API URL
+function generateApiUrl(cityName, apiKey) {
+    // Construct the API URL using provided cityName and apiKey
+    return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+}
+
+// Define API key and city
 const apiKey = 'f60b45c3793f30b73bc97da716ed83f1';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm,Sweden&units=metric&APPID=${apiKey}`;
+const city = 'Stockholm';
 
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        const city = data.name;
-        const temperature = data.main.temp.toFixed(1);
-        const description = data.weather[0].description;
 
-        document.getElementById('city').textContent = `City: ${city}`;
-        document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
-        document.getElementById('description').textContent = `Description: ${description}`;
-    })
-    .catch(error => console.error('Error:', error));
+    document.getElementById('submit-button').addEventListener('click', function() {
+        const cityName = document.getElementById('city-input').value;
+        const newApiUrl = generateApiUrl(cityName, apiKey);
+    
+        fetch(newApiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const city = data.name;
+                const temperature = data.main.temp.toFixed(1);
+                const description = data.weather[0].description;
+    
+                document.getElementById('city').textContent = `City: ${city}`;
+                document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
+                document.getElementById('description').textContent = `Description: ${description}`;
+            })
+            .catch(error => console.error('Error:', error));
+    });
+    
