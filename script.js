c9830c74248827fdf0f4e201590d3b821f0905aa
Behrouz87RN <112555779+Behrouz87RN@users.noreply.github.com>
2023-10-12 20:06:40 +0200

    HTml updated

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Function to generate API URL
-function generateApiUrl(cityName, apiKey) {
-    return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
-}
-
-// Define API key and city
-const apiKey = 'f60b45c3793f30b73bc97da716ed83f1';
-const city = 'Stockholm';
 
 document.getElementById('submit-button').addEventListener('click', function() {
     const cityName = document.getElementById('city-input').value;
-    const newApiUrl = generateApiUrl(cityName, apiKey);
+    const apiKey = 'f60b45c3793f30b73bc97da716ed83f1';
 
-    fetch(newApiUrl)
+    // Function to generate API URLs
+    function generateApiUrl(type) {
+        return `https://api.openweathermap.org/data/2.5/${type}?q=${cityName}&units=metric&APPID=${apiKey}`;
+    }
+
+    // Fetch current weather data
+    fetch(generateApiUrl('weather'))
         .then(response => response.json())
         .then(data => {
             const city = data.name;
@@ -21,31 +19,29 @@ document.getElementById('submit-button').addEventListener('click', function() {
             document.getElementById('city').textContent = `City: ${city}`;
             document.getElementById('temperature').textContent = `Temperature: ${temperature}°C`;
             document.getElementById('description').textContent = `Description: ${description}`;
+        })
+        .catch(error => console.error('Error:', error));
 
-            // Fetch and display forecast data
-            const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+    // Fetch forecast data
+    fetch(generateApiUrl('forecast'))
+        .then(response => response.json())
+        .then(data => {
+            const forecastList = data.list;
+            const forecastContainer = document.getElementById('forecast-list');
+            forecastContainer.innerHTML = ''; // Clear previous forecast data
 
-            fetch(forecastUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const forecastList = data.list;
-                    const forecastContainer = document.getElementById('forecast-list');
-                    forecastContainer.innerHTML = ''; // Clear previous forecast data
+            forecastList.forEach(item => {
+                const date = new Date(item.dt * 1000);
+                const dateString = date.toDateString();
+                const minTemp = item.main.temp_min.toFixed(1);
+                const maxTemp = item.main.temp_max.toFixed(1);
+                const description = item.weather[0].description;
 
-                    forecastList.forEach(item => {
-                        const date = new Date(item.dt * 1000);
-                        const dateString = date.toDateString();
-                        const minTemp = item.main.temp_min.toFixed(1);
-                        const maxTemp = item.main.temp_max.toFixed(1);
-                        const description = item.weather[0].description;
+                const listItem = document.createElement('li');
+                listItem.textContent = `${dateString}: Min Temp: ${minTemp}°C, Max Temp: ${maxTemp}°C, Description: ${description}`;
 
-                        const listItem = document.createElement('li');
-                        listItem.textContent = `${dateString}: Min Temp: ${minTemp}°C, Max Temp: ${maxTemp}°C, Description: ${description}`;
-
-                        forecastContainer.appendChild(listItem);
-                    });
-                })
-                .catch(error => console.error('Error:', error));
+                forecastContainer.appendChild(listItem);
+            });
         })
         .catch(error => console.error('Error:', error));
 });
